8fd50bccd81ac27f8868b3038d90353c26ae4ef3
Manvi Dewangan <109518835+Manvi1902@users.noreply.github.com>
2024-10-05 13:29:19 +0530

    Add files via upload

---
+++ models/blogModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema(
+    {
+    title:{
+        type:String,
+        require:[true,'title is required']
+    },
+    description:{
+        type:String,
+        require:[true,'description is required']
+    },
+    image:{
+        type:String,
+        require:[true,'image is require']
+    },
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:"user",
+        require:[true,"user id is required"],
+    }
+
+
+},{timestamps:true});
+
+const blogModel = mongoose.model('Blog',blogSchema);
+
+module.exports = blogModel;
